fb29f72119b995c803f93b5c74bc196dfa77e0be
Joshua Thompson <warlord84502@yahoo.com>
2026-01-23 12:17:36 -0600

    backend/compliance-models-scaffold

---
+++ backend/services/compliance_event_service/src/main/java/com/skillstorm/finsight/compliance_event/models/ComplianceEventSarDetail.java
@@ -0,0 +1,10 @@
+package com.skillstorm.finsight.compliance_event.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "compliance_event_sar_detail", schema = "compliance_event")
+public class ComplianceEventSarDetail {
+
+}
---
+++ backend/services/compliance_event_service/src/main/java/com/skillstorm/finsight/compliance_event/models/EventType.java
@@ -0,0 +1,5 @@
+package com.skillstorm.finsight.compliance_event.models;
+
+public class EventType {
+    
+}
