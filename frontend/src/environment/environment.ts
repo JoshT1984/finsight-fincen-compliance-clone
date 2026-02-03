4e6560546adbf73089ed936ef04b4ff2e25527a6
Matthew Wright <matthew.wright9630@gmail.com>
2026-02-03 17:40:03 -0500

    Remove duplicate variables

--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -6,7 +6,6 @@ export const environment = {
   production: false,
   documentsApiBaseUrl: 'http://localhost:8081',
   suspectApiBaseUrl: 'http://localhost:8082',
-  complianceApiBaseUrl: 'http://localhost:8085',
   /** Identity and Authorization microservice (identity-auth-service). No trailing slash. */
   identityApiBaseUrl: 'http://localhost:8083',
   /** Compliance event microservice (compliance-event-service). No trailing slash. */
