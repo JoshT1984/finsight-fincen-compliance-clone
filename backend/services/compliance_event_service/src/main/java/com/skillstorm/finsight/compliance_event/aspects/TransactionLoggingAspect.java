ca8115869fa4a9b3d925cbfabde5290a6fb111ff
Matthew Wright <matthew.wright9630@gmail.com>
2026-02-10 22:40:22 -0500

    Add loggers to compliance_events. Update the logging to be more uniform in the Identity service.

---
+++ backend/services/compliance_event_service/src/main/java/com/skillstorm/finsight/compliance_event/aspects/TransactionLoggingAspect.java
@@ -0,0 +1,5 @@
+package com.skillstorm.finsight.compliance_event.aspects;
+
+public class TransactionLoggingAspect {
+
+}
---
+++ backend/services/compliance_event_service/src/main/java/com/skillstorm/finsight/compliance_event/enums/AuditAction.java
@@ -0,0 +1,14 @@
+package com.skillstorm.finsight.compliance_event.enums;
+
+public enum AuditAction {
+    CREATE_CTR,
+    CREATE_SAR,
+    UPDATE,
+    DEACTIVATE,
+    LOGIN,
+    LINK,
+    UNLINK,
+    PASSWORD_RESET_EMAIL,
+    PASSWORD_RESET,
+    LOGOUT
+}
